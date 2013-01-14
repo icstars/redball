7c7f6889c7e861e3581c076bbdd04bde52f61f6f
icstars <appldev@icstars.org>
2013-01-14 10:31:28 -0800

    Create highscores.php

---
+++ highscores.php
@@ -0,0 +1,45 @@
+<?php
+$user = 'icstars_reduser';
+$pwd = '6IL934;-[!}?';
+$db = 'icstars_redball';
+  $link = mysql_connect('localhost', $user, $pwd);
+  if(!$link) {
+    die('Could not connect: ' . mysql_error());
+  } else {
+    mysql_select_db($db, $link);
+    $query = <<<SQL
+select * from highscore;
+SQL;
+
+    $result = mysql_query($query, $link);
+  }
+  
+    $output = array(
+  	"aaData" => array()
+	);
+  
+$aColumns = array( 'id', 'username', 'score', 'thedate');
+  while ( $aRow = mysql_fetch_array( $result ) )
+	{
+		$row = array();
+		for ( $i=0 ; $i<count($aColumns) ; $i++ )
+		{
+			if ( $aColumns[$i] == "version" )
+			{
+				/* Special output formatting for 'version' column */
+				$row[] = ($aRow[ $aColumns[$i] ]=="0") ? '-' : $aRow[ $aColumns[$i] ];
+			}
+			else if ( $aColumns[$i] != ' ' )
+			{
+				/* General output */
+				$row[] = $aRow[ $aColumns[$i] ];
+			}
+		}
+		$output['aaData'][] = $row;
+	}
+	
+	echo json_encode( $output );
+
+
+
+?>
